6a361687b634f668e47615d5857fb123320af7c6
Tayeeb Hasan <tayeeb.hasan@gmail.com>
2023-01-21 11:49:44 -0500

    add user model

---
+++ models/user.model.js
@@ -0,0 +1,37 @@
+import mongoose from 'mongoose';
+import bcrypt from "bcryptjs";
+
+const userSchema = mongoose.Schema({
+    email: {
+        type: String,
+        required: [true, "email is required"],
+        lowercase: true,
+        unique: true,
+        match: [/^\w+([\.-]?\w+)*@\w+([\.-]?\w+)*(\.\w{2,3})+$/, 'Please enter a valid email address']
+    },
+    password: {
+        type: String,
+        required: [true, 'A password is required'],
+        minLength: 8,
+    },
+    name: {
+        type: String,
+        required: true,
+        unique: true
+    },
+}, { timestamps: true })
+
+userSchema.methods.matchPassword = async function (enteredPassword) {
+    return await bcrypt.compare(enteredPassword, this.password);
+};
+
+userSchema.pre("save", async function (next) {
+    if (!this.isModified('password')) {
+        next();
+    };
+    const salt = await bcrypt.genSalt(10);
+    this.password = await bcrypt.hash(this.password, salt);
+});
+
+const Users = mongoose.model("Users", userSchema);
+export default Users;
