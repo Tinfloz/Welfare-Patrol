2cc0bf18f804d96550eac0407c1edac1bb9a6475
Tayeeb Hasan <tayeeb.hasan@gmail.com>
2023-01-21 22:29:36 -0500

    integrate chats api

--- frontend/src/pages/Messages.jsx
+++ frontend/src/pages/Messages.jsx
@@ -1,4 +1,4 @@
-import React from 'react';
+import React, {useEffect, useState} from 'react';
 import {
   VStack,
   Flex,
@@ -8,16 +8,54 @@ import {
   InputGroup,
   InputLeftElement,
   IconButton,
+  Icon,
+  Box,
+  Circle
 } from '@chakra-ui/react';
 import { AiOutlineSearch } from 'react-icons/ai';
 import { BiCheckDouble } from 'react-icons/bi';
+import { BsFillCircleFill } from 'react-icons/bs';
+import fetchApi from "../components/FetchCustom";
+import { useNavigate } from 'react-router-dom';
 
 const Messages = () => {
+  const [chats, setChats] = useState([]);
+  const navigate = useNavigate();
+  useEffect(()=>{
+    const token = localStorage.getItem("welfarePatrol-user");
+    const fn = () => {
+      fetchApi("/api/chats", {
+        method: "get",
+        headers: {
+          "Content-Type": "application/json",
+          Authorization: `Bearer ${token}`,
+        },
+      })
+      .then((res) => res.json())
+      .then((json) => {
+        if (json.chats){
+          setChats(json.chats);
+        } else {
+          console.error("Failed!");
+        }
+      })
+      .catch((err) => {
+        console.log(err);
+      });
+    }
+    if (token){
+      fn();
+    } else {
+      navigate("/login")
+    }
+  },[navigate])
+
+
   return (
     <>
-      <Flex height="90vh" width="100%" justifyContent="center" mt="10vh">
-        <VStack spacing="3vh" align="stretch">
-          <Text as="b" fontSize="3vh">
+      <Flex height="90vh"  justifyContent="center" mt="10vh" width={"100vw"}>
+        <VStack spacing="2vh" align="left">
+          <Text as="b" fontSize="3vh" align={"left"}>
             Messages
           </Text>
           <InputGroup>
@@ -28,38 +66,62 @@ const Messages = () => {
             <Input type="text" placeholder="Search" />
           </InputGroup>
           <Divider orientation="horizontal" />
-          <Flex>
-            <Text as="b" fontSize="2vh">
-              Dave Howard
-            </Text>
-            <Text fontSize="2vh" marginLeft="20vh">
-              9:28 PM
-            </Text>
-          </Flex>
-          <Flex>
-            <IconButton
-              icon={<BiCheckDouble size="3vh" />}
-              style={{ background: 'white', paddingBottom: '15px' }}
-            />
-            <Text fontSize="2vh">I can drop in about 30 minutes</Text>
-          </Flex>
-          <Divider orientation="horizontal" />
-          <Flex>
-            <Text as="b" fontSize="2vh">
-              John Kelly
-            </Text>
-            <Text fontSize="2vh" marginLeft="20vh">
-              Jan 02
-            </Text>
-          </Flex>
-          <Flex>
-            <Text fontSize="2vh">Thank you so much she just called...</Text>
-          </Flex>
-          <Divider orientation="horizontal" />
+          {
+            chats.map(({chat, lastMessage}, id)=>{
+              let date = new Date(lastMessage.createdAt);
+              return (
+                <MessageBlock
+                key={id}
+                name={chat.sender.name} 
+                time={`${date.getHours()}:${date.getMinutes()}`} 
+                message={lastMessage.content} 
+                sent={lastMessage.sender === chat.receiver["_id"]}/>
+              )
+            })
+          }
         </VStack>
       </Flex>
     </>
   );
 };
+
+const MessageBlock = ({name, time, message, sent}) => {
+  return (         
+  <VStack spacing={"1vh"} align={"left"} width="100%">
+   <Flex width="100%" justifyContent={"space-between"} marginTop={"0px"}>
+    <Text as="b" fontSize="2vh">
+      {name}
+    </Text>
+    <Text fontSize="2vh">
+      {time}
+    </Text>
+  </Flex>
+  <Flex width="100%" justifyContent="flex-start">
+    {
+      sent ? (
+        <>
+        <Icon
+        as={BiCheckDouble}
+        marginRight="1vh"
+        />
+        <Text fontSize="2vh" as={`${sent ? "": "b"}`} color="gray.600">{message}</Text>
+        </>
+      ):(<Flex justifyContent={"space-between"} alignItems="center" width={"100%"}>
+        <Text fontSize="2vh" as={`${sent ? "": "b"}`} color="gray.600">{message}</Text>
+
+          <Icon
+          as={BsFillCircleFill}
+          fill="red.200"
+          bg="red.200"
+          borderRadius="50%"
+          fontSize={"1vh"}
+          />
+      </Flex>)
+    }
+  </Flex>
+  <Divider orientation="horizontal" />
+  </VStack>
+  )
+}
 
 export default Messages;
