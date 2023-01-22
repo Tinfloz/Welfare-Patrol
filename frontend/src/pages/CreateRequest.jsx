589825a9bb2ec1d71e23d7215269034f28119420
Ali Affan <affan.ali368@gmail.com>
2023-01-22 03:36:57 -0500

    Add AutoComplete Component to CreateRequest Screen

--- frontend/src/pages/CreateRequest.jsx
+++ frontend/src/pages/CreateRequest.jsx
@@ -1,91 +1,114 @@
-import React from 'react';
-import { Flex, VStack, Text, Box, Heading, Input, Button, Spinner } from '@chakra-ui/react';
+import React, { useState } from 'react';
+import {
+  Flex,
+  VStack,
+  Text,
+  Box,
+  Heading,
+  Input,
+  Button,
+  Spinner,
+} from '@chakra-ui/react';
 import { useJsApiLoader, GoogleMap, Marker } from '@react-google-maps/api';
+import Autocomplete from 'react-google-autocomplete';
 
 const CreateRequest = () => {
+  const user = localStorage.getItem('user');
+  console.log(user.token);
+  const [welfareRequest, setWelfareRequest] = useState({
+    token: '',
+    address: '',
+    coordinateA: '',
+    coordinateB: '',
+  });
+  const { isLoaded } = useJsApiLoader({
+    googleMapsApiKey: process.env.REACT_APP_GOOGLE_MAPS_API_KEY,
+    libraries: ['places'],
+  });
 
-    const { isLoaded } = useJsApiLoader({
-        googleMapsApiKey: process.env.REACT_APP_GOOGLE_MAPS_API_KEY
-    })
-
-    return (
+  return (
+    <>
+      {!isLoaded ? (
         <>
-            {
-                !isLoaded ? (
-                    <>
-                        <Flex
-                            justify="center"
-                            alignItems="Center"
-                            p="20vh"
-                        >
-                            <Spinner
-                                speed="0.65s"
-                                thickness='4px'
-                                color='blue.500'
-                                emptyColor='gray.300'
-                                size="xl"
-                            />
-                        </Flex>
-                    </>
-                ) : (
-                    <>
-                        {/* <VStack spacing="-10vh"> */}
-                        <Box
-                            h="60vh"
-                            w="100%"
-                            position="relative"
-                            zIndex="-10"
-                        >
-                            <GoogleMap
-                                center={{ lat: 45.5307609, lng: -73.5526934 }}
-                                zoom={15}
-                                mapContainerStyle={{ width: "100%", height: "60vh" }}
-                                options={{ mapTypeControl: false, zoomControl: false, streetViewControl: false, fullscreenControl: false }}
-                            >
-                                <Marker
-                                    position={{ lat: 45.5307609, lng: -73.5526934 }}
-                                />
-                            </GoogleMap>
-                        </Box>
-                        <Box
-                            h="39vh"
-                            borderTop="gray.300"
-                            borderTopWidth="1px"
-                            borderTopColor="gray.300"
-                            borderTopRadius="10vh"
-                            w="100%"
-                            justify="center"
-                            alignItems="center"
-                            zIndex="1"
-                        >
-                            <VStack p="3vh" spacing="5vh">
-                                <Flex
-                                    justify="center"
-                                    alignItems="center"
-                                >
-                                    <Heading size="md">
-                                        Montreal , QC
-                                    </Heading>
-                                </Flex>
-                                <Input type="text" placeholder="Type address" w="40vh" />
-                                <Button
-                                    w="50vh"
-                                    borderRadius="2vh"
-                                    borderWidth="1px"
-                                    borderColor="gray.300"
-                                    style={{ background: "#F8D9D2" }}
-                                    color="white"
-                                >
-                                    CREATE REQUEST
-                                </Button>
-                            </VStack>
-                        </Box>
-                        {/* </VStack> */}
-                    </>
-                )
-            }
+          <Flex justify="center" alignItems="Center" p="20vh">
+            <Spinner
+              speed="0.65s"
+              thickness="4px"
+              color="blue.500"
+              emptyColor="gray.300"
+              size="xl"
+            />
+          </Flex>
         </>
-    )
-}
+      ) : (
+        <>
+          {/* <VStack spacing="-10vh"> */}
+          <Box h="60vh" w="100%" position="relative" zIndex="-10">
+            <GoogleMap
+              center={{ lat: 45.5307609, lng: -73.5526934 }}
+              zoom={15}
+              mapContainerStyle={{ width: '100%', height: '60vh' }}
+              options={{
+                mapTypeControl: false,
+                zoomControl: false,
+                streetViewControl: false,
+                fullscreenControl: false,
+              }}
+            >
+              <Marker position={{ lat: 45.5307609, lng: -73.5526934 }} />
+            </GoogleMap>
+          </Box>
+          <Box
+            h="39vh"
+            borderTop="gray.300"
+            borderTopWidth="1px"
+            borderTopColor="gray.300"
+            borderTopRadius="10vh"
+            w="100%"
+            justify="center"
+            alignItems="center"
+            zIndex="1"
+          >
+            <VStack p="3vh" spacing="5vh">
+              <Flex justify="center" alignItems="center">
+                <Heading size="md">Montreal , QC</Heading>
+              </Flex>
+              {/* <Input
+                type="text"
+                placeholder="Type address"
+                w="40vh"
+                onChange={autoComplete}
+              /> */}
+              <Autocomplete
+                apiKey={process.env.REACT_APP_GOOGLE_MAPS_API_KEY}
+                style={{ width: '50vh' }}
+                onPlaceSelected={place => {
+                  console.log(place);
+                }}
+                options={{
+                  types: ['(regions)'],
+                  componentRestrictions: { country: 'ca' },
+                }}
+                defaultValue="Hall Building"
+              />
+              ;
+              <Button
+                w="50vh"
+                borderRadius="2vh"
+                borderWidth="1px"
+                borderColor="gray.300"
+                style={{ background: '#F8D9D2' }}
+                color="white"
+              >
+                CREATE REQUEST
+              </Button>
+            </VStack>
+          </Box>
+          {/* </VStack> */}
+        </>
+      )}
+    </>
+  );
+};
 
-export default CreateRequest
+export default CreateRequest;
